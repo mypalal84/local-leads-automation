a4d9af912635aa9e4d851e650c140af05b28db25
mypalal84 <alexcahn@gmail.com>
2026-02-01 12:31:08 -0800

    Initial commit: organized structure inside existing Daily_Leads folder

---
+++ scripts/run_leads.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+export SERPER_API_KEY="your_real_serper_key"
+export SERPAPI_API_KEY="your_real_serpapi_key"
+export HUNTER_API_KEY="your_real_hunter_key"
+export GOOGLE_PLACES_API_KEY="your_real_google_places_key"
+export DAILY_LEAD_EMAIL_SENDER="your_gmail@gmail.com"
+export DAILY_LEAD_EMAIL_PASS="your_app_password"
+export DAILY_LEAD_EMAIL_TO="your_gmail@gmail.com"
+
+/Library/Frameworks/Python.framework/Versions/3.14/bin/python3 \
+  /Users/alexcahn/Scripts/Daily_Leads/lead_generator_no_website_enriched.py \
+  >> /Users/alexcahn/Scripts/Daily_Leads/daily_run.log \
+  2>> /Users/alexcahn/Scripts/Daily_Leads/daily_run_error.log
